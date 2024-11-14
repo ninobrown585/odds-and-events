1786f14025cd27097fdceff2e3088610a8b6f5e4
Adrian Brown <ninobrown58518@gmail.com>
2024-11-14 14:12:17 -0600

    finally able to send input value to number bank

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // TODO: this file! :)
+
+const form = document.querySelector('form');
+const addNumber = document.querySelector('#addNumber');
+const sort1 = document.querySelector('#sortOne');
+const sortAll = document.querySelector('#sortAll');
+const output = document.querySelector('output');
+const arrNumbers = [];
+
+const printNumbers = () => {
+    output.innerHTML = arrNumbers;
+//    console.log(tester);
+    
+};
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const number = new FormData(e.target);
+    const numberValue = number.get('number');
+    // console.log(numberValue);
+    arrNumbers.push(numberValue);
+    printNumbers();
+    addNumber.value = '';
+});
+
+sort1.addEventListener('click', function() {
+    arrNumbers.sort((a, b) => a - b);
+    printNumbers();
+});
+
+sortAll.addEventListener('click', function() {
+    arrNumbers.sort((a, b) => a - b);
+    printNumbers();
+});
+
+
+
