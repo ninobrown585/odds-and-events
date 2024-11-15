22a0b8662af1cde80051ea76407f647de903ef3f
Adrian Brown <ninobrown58518@gmail.com>
2024-11-14 22:12:33 -0600

    finally

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const addNumber = document.querySelector('#addNumber');
 const sort1 = document.querySelector('#sortOne');
 const sortAll = document.querySelector('#sortAll');
 const output = document.querySelector('output');
+const oddResult = document.querySelector('#odds output');
+const evenResult = document.querySelector('#evens output');
 const arrNumbers = [];
 
 const printNumbers = () => {
@@ -25,12 +27,38 @@ form.addEventListener('submit', function(e) {
 
 sort1.addEventListener('click', function() {
     arrNumbers.sort((a, b) => a - b);
-    printNumbers();
+    //filter out the odd and even numbers
+    
+    // printNumbers();
+    if (arrNumbers.length > 0) {
+        const number = arrNumbers[0];
+        if (number % 2 === 0) {
+            evenResult.innerHTML += number;
+        } else {
+            oddResult.innerHTML += number;   
+        }
+        arrNumbers.shift();
+        
+        printNumbers();
+        addNumber.value = '';
+        
+    }
 });
 
 sortAll.addEventListener('click', function() {
     arrNumbers.sort((a, b) => a - b);
+
+    for (const number of arrNumbers) {
+        if (number % 2 === 0) {
+            evenResult.innerHTML += number;
+        } else {
+            oddResult.innerHTML += number;   
+        }
+    }
+    arrNumbers.length = 0;
     printNumbers();
+    addNumber.value = '';
+    
 });
 
 
